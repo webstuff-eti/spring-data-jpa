bdc8a4443cff3b83cd4302c516e310e4d63aff87
Tiago Poliano Tibaes <tiago.tibaes@cwi.com.br>
2019-04-17 12:08:46 -0300

    criação do projeto inicial Spring-Data-Jpa

---
+++ iniciando-com-spring-data-jpa/src/main/java/br/eti/webstuff/iniciandocomspringdatajpa/IniciandoComSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package br.eti.webstuff.iniciandocomspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniciandoComSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniciandoComSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ iniciando-com-spring-data-jpa/src/test/java/br/eti/webstuff/iniciandocomspringdatajpa/IniciandoComSpringDataJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.eti.webstuff.iniciandocomspringdatajpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IniciandoComSpringDataJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
